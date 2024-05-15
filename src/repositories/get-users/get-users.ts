186da14576f978af143dbbaef4cb277625c3ab48
Matheus Medeiros <promatheusmedeiros@gmail.com>
2024-05-15 20:02:30 -0300

    feat: make MongoGetUsersRepository gets users from mongo

--- src/repositories/get-users/get-users.ts
+++ src/repositories/get-users/get-users.ts
@@ -1,15 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/User";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "matheus",
-        lastName: "oliveira",
-        email: "email@email.com",
-        password: "senha",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
