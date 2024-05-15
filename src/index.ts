e8f17c375250051a0414109c45c60fb6cca1ee2f
Matheus Medeiros <promatheusmedeiros@gmail.com>
2024-05-15 18:44:44 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-function amor() {
-  return { 2: 3 };
-}
+import express from "express";
+import "dotenv/config";
+
+const server = express();
+
+const PORT = process.env.PORT ?? 8000;
+
+server.listen(PORT, () => console.log(`Server Running: PORT ${PORT}`));
