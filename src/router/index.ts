9b60c3be91994cd81e08711ae0fe242d1e6d4446
Matheus Medeiros <promatheusmedeiros@gmail.com>
2024-05-21 18:09:47 -0300

    feat: add DeleteUserController and router

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,8 @@ import { MongoCreateUserRepository } from "../repositories/create-user/mongo-cre
 import { CreateUserController } from "../controllers/create-user/create-user";
 import { MongoUpdateUserRepository } from "../repositories/update-user/mongo-update-user";
 import { UpdateUserController } from "../controllers/update-user/update-user";
+import { MongoDeleteUserRepository } from "../repositories/delete-user/mongo-delete-user";
+import { DeleteUserController } from "../controllers/delete-user/delete-user";
 
 const router = Router();
 
@@ -37,6 +39,19 @@ router.patch("/users/:id", async (req, res) => {
 
   const { body, statusCode } = await updateUserController.handle({
     body: req.body,
+    params: req.params,
+  });
+
+  res.status(statusCode).send(body);
+});
+
+router.delete("/users/:id", async (req, res) => {
+  const mongoDeleteUserRepository = new MongoDeleteUserRepository();
+  const deleteUserController = new DeleteUserController(
+    mongoDeleteUserRepository
+  );
+
+  const { body, statusCode } = await deleteUserController.handle({
     params: req.params,
   });
 
